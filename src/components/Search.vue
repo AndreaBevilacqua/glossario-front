5accff0667f5a85c5d15e68d0bfb878b30fe212d
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-04-08 13:03:02 +0200

    add filter

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,26 @@
 <script>
 export default {
     name: 'Search',
-    data: () => ({ inputValue: '', timeoutId: null }),
+    data() {
+        return {
+            inputValue: '',
+            timeoutId: null
+        };
+    },
     methods: {
         search() {
             clearTimeout(this.timeoutId);
-            this.timeoutId = setTimeout(() => { this.$emit('search', this.inputValue); }, 500);
-        }
+            this.timeoutId = setTimeout(() => {
+                this.$emit('search', this.inputValue.trim());
+            }, 1000);
+        },
     }
 };
 </script>
 
 <template>
-    <input type="text" placeholder="Cerca una parola" v-model="inputValue" @keyup="search" />
+    <input type="text" placeholder="Cerca una parola" v-model="inputValue" @keyup="search"
+        @keydown.enter.prevent="handleEnter" />
 </template>
 
 <style></style>
